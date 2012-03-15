b40a0dccac017ecf086bbc3594c3c6b4d5ab634f
Dale Henrichs <dhenrich@vmware.com>
2012-03-14 21:39:27 -0700

    blueprint doesn't conflict with Amber class browser method menus

--- js/AmberProjectPage.deploy.js
+++ js/AmberProjectPage.deploy.js
@@ -6,7 +6,7 @@ smalltalk.method({
 selector: unescape('renderOn%3A'),
 fn: function (html){
 var self=this;
-smalltalk.send(html, "_with_", ["This is your Amber project template skeleton page."]);
+(function($rec){smalltalk.send($rec, "_class_", ["container"]);return smalltalk.send($rec, "_with_", [(function(){smalltalk.send(html, "_h1_", [(function(h){return smalltalk.send(h, "_with_", ["Amber Project Name"]);})]);smalltalk.send(html, "_h3_", [(function(h){return smalltalk.send(h, "_with_", ["Amber Project description"]);})]);return smalltalk.send(html, "_with_", ["This is your Amber project template skeleton page."]);})]);})(smalltalk.send(html, "_div", []));
 return self;}
 }),
 smalltalk.AmberProjectPage);
