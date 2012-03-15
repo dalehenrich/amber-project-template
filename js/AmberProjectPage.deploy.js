fdd1c30f49ec5349546e3127adfdd1683579026f
Dale Henrichs <dhenrich@vmware.com>
2012-03-14 21:44:55 -0700

    checkpoint

--- js/AmberProjectPage.deploy.js
+++ js/AmberProjectPage.deploy.js
@@ -6,7 +6,7 @@ smalltalk.method({
 selector: unescape('renderOn%3A'),
 fn: function (html){
 var self=this;
-(function($rec){smalltalk.send($rec, "_class_", ["container"]);return smalltalk.send($rec, "_with_", [(function(){smalltalk.send(html, "_h1_", [(function(h){return smalltalk.send(h, "_with_", ["Amber Project Name"]);})]);smalltalk.send(html, "_h3_", [(function(h){return smalltalk.send(h, "_with_", ["Amber Project description"]);})]);return smalltalk.send(html, "_with_", ["This is your Amber project template skeleton page."]);})]);})(smalltalk.send(html, "_div", []));
+(function($rec){smalltalk.send($rec, "_class_", ["container"]);return smalltalk.send($rec, "_with_", [(function(){(function($rec){smalltalk.send($rec, "_id_", ["header"]);smalltalk.send($rec, "_class_", [unescape("span-24%20last")]);return smalltalk.send($rec, "_with_", [(function(h){return smalltalk.send(h, "_with_", ["Amber Project Name"]);})]);})(smalltalk.send(html, "_h1", []));(function($rec){smalltalk.send($rec, "_id_", ["subheader"]);smalltalk.send($rec, "_class_", [unescape("span-24%20last")]);return smalltalk.send($rec, "_with_", [(function(h){return smalltalk.send(h, "_with_", ["Amber Project description"]);})]);})(smalltalk.send(html, "_h3", []));return smalltalk.send(html, "_with_", ["This is your Amber project template skeleton page."]);})]);})(smalltalk.send(html, "_div", []));
 return self;}
 }),
 smalltalk.AmberProjectPage);
