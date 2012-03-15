7dd5cc9818f075971bbc31642d69d4d83fceac34
Dale Henrichs <dhenrich@vmware.com>
2012-03-15 00:03:34 -0700

    extract description using api.github.com

--- js/AmberProjectPage.deploy.js
+++ js/AmberProjectPage.deploy.js
@@ -1,13 +1,12 @@
 smalltalk.addPackage('AmberProjectPage', {});
-smalltalk.addClass('AmberProjectPage', smalltalk.Widget, ['repositoryDiv', 'username', 'projectname', 'descriptionDiv'], 'AmberProjectPage');
+smalltalk.addClass('AmberProjectPage', smalltalk.Widget, ['repositoryDiv', 'username', 'projectname', 'descriptionDiv', 'repositoryJson', 'requestStatus'], 'AmberProjectPage');
 smalltalk.addMethod(
 unescape('_renderOn_'),
 smalltalk.method({
 selector: unescape('renderOn%3A'),
 fn: function (html){
 var self=this;
-(function($rec){smalltalk.send($rec, "_class_", ["container"]);return smalltalk.send($rec, "_with_", [(function(){(function($rec){smalltalk.send($rec, "_id_", ["header"]);smalltalk.send($rec, "_class_", [unescape("span-24%20last")]);return smalltalk.send($rec, "_with_", [(function(div){return smalltalk.send(div, "_h1_", [smalltalk.send(self, "_projectname", [])]);})]);})(smalltalk.send(html, "_div", []));smalltalk.send(html, "_hr", []);(function($rec){smalltalk.send($rec, "_id_", ["subheader"]);smalltalk.send($rec, "_class_", [unescape("span-24%20last")]);return smalltalk.send($rec, "_with_", [(function(div){return (self['@descriptionDiv']=(function($rec){smalltalk.send($rec, "_class_", ["alt"]);smalltalk.send($rec, "_with_", ["Amber Project description"]);return smalltalk.send($rec, "_yourself", []);})(smalltalk.send(div, "_h3", [])));})]);})(smalltalk.send(html, "_div", []));smalltalk.send(html, "_hr", []);smalltalk.send(html, "_with_", ["This is your Amber project README."]);(self['@repositoryDiv']=smalltalk.send(smalltalk.send(html, "_div", []), "_id_", ["test"]));return smalltalk.send(smalltalk.send(html, "_div", []), "_with_", [(function(){return (function($rec){smalltalk.send($rec, "_class_", ["test"]);smalltalk.send($rec, "_with_", ["test get"]);return smalltalk.send($rec, "_onClick_", [(function(){return smalltalk.send(self['@repositoryDiv'], "_contents_", [(function(div){return smalltalk.send(self, "_getTest", []);})]);})]);})(smalltalk.send(html, "_button", []));})]);})]);})(smalltalk.send(html, "_div", []));
-smalltalk.send(self, "_getRepositoryJsonDo_onError_", [(function(repoJSON){return smalltalk.send(self['@descriptionDiv'], "_contents_", [(function(h){return smalltalk.send(h, "_with_", [smalltalk.send(repoJSON, "_description", [])]);})]);}), (function(status){return nil;})]);
+(function($rec){smalltalk.send($rec, "_class_", ["container"]);return smalltalk.send($rec, "_with_", [(function(){(function($rec){smalltalk.send($rec, "_id_", ["header"]);smalltalk.send($rec, "_class_", [unescape("span-24%20last")]);return smalltalk.send($rec, "_with_", [(function(div){return smalltalk.send(div, "_h1_", [smalltalk.send(self, "_projectname", [])]);})]);})(smalltalk.send(html, "_div", []));smalltalk.send(html, "_hr", []);(function($rec){smalltalk.send($rec, "_id_", ["subheader"]);smalltalk.send($rec, "_class_", [unescape("span-24%20last")]);return smalltalk.send($rec, "_with_", [(function(div){(self['@descriptionDiv']=(function($rec){smalltalk.send($rec, "_class_", ["alt"]);return smalltalk.send($rec, "_yourself", []);})(smalltalk.send(div, "_h3", [])));return smalltalk.send(self, "_repositoryJsonDo_", [(function(json){return smalltalk.send(self['@descriptionDiv'], "_with_", [smalltalk.send(json, "_description", [])]);})]);})]);})(smalltalk.send(html, "_div", []));smalltalk.send(html, "_hr", []);smalltalk.send(html, "_with_", ["This is your Amber project README."]);(self['@repositoryDiv']=smalltalk.send(smalltalk.send(html, "_div", []), "_id_", ["test"]));return smalltalk.send(smalltalk.send(html, "_div", []), "_with_", [(function(){return (function($rec){smalltalk.send($rec, "_class_", ["test"]);smalltalk.send($rec, "_with_", ["test get"]);return smalltalk.send($rec, "_onClick_", [(function(){return smalltalk.send(self['@repositoryDiv'], "_contents_", [(function(div){return smalltalk.send(self, "_getTest", []);})]);})]);})(smalltalk.send(html, "_button", []));})]);})]);})(smalltalk.send(html, "_div", []));
 return self;}
 }),
 smalltalk.AmberProjectPage);
@@ -57,6 +56,18 @@ selector: unescape('getRepositoryJsonDo%3AonError%3A'),
 fn: function (aBlock, errorBlock){
 var self=this;
 smalltalk.send((typeof jQuery == 'undefined' ? nil : jQuery), "_ajax_options_", [smalltalk.send(smalltalk.send(smalltalk.send(unescape("https%3A/api.github.com/repos/"), "__comma", [smalltalk.send(self, "_username", [])]), "__comma", [unescape("/")]), "__comma", [smalltalk.send(self, "_projectname", [])]), smalltalk.HashedCollection._fromPairs_([smalltalk.send("type", "__minus_gt", ["GET"]),smalltalk.send("dataType", "__minus_gt", ["jsonp"]),smalltalk.send("success", "__minus_gt", [(function(repositoryData, status, jqXHR){return smalltalk.send(aBlock, "_value_", [smalltalk.send(repositoryData, "_data", [])]);})]),smalltalk.send("error", "__minus_gt", [(function(jqXHR, status, error){return smalltalk.send(errorBlock, "_value_", [smalltalk.send(status, "_printString", [])]);})])])]);
+return self;}
+}),
+smalltalk.AmberProjectPage);
+
+smalltalk.addMethod(
+unescape('_repositoryJsonDo_'),
+smalltalk.method({
+selector: unescape('repositoryJsonDo%3A'),
+fn: function (aBlock){
+var self=this;
+(($receiver = self['@repositoryJson']) == nil || $receiver == undefined) ? (function(){return smalltalk.send(self, "_getRepositoryJsonDo_onError_", [(function(repoJSON){(self['@repositoryJson']=repoJSON);return smalltalk.send(aBlock, "_value_", [self['@repositoryJson']]);}), (function(status){return nil;})]);})() : $receiver;
+smalltalk.send(smalltalk.send(aBlock, "_value", []), "_repositoryJson", []);
 return self;}
 }),
 smalltalk.AmberProjectPage);
--- js/AmberProjectPage.js
+++ js/AmberProjectPage.js
@@ -1,5 +1,5 @@
 smalltalk.addPackage('AmberProjectPage', {});
-smalltalk.addClass('AmberProjectPage', smalltalk.Widget, ['repositoryDiv', 'username', 'projectname', 'descriptionDiv'], 'AmberProjectPage');
+smalltalk.addClass('AmberProjectPage', smalltalk.Widget, ['repositoryDiv', 'username', 'projectname', 'descriptionDiv', 'repositoryJson', 'requestStatus'], 'AmberProjectPage');
 smalltalk.addMethod(
 unescape('_renderOn_'),
 smalltalk.method({
@@ -7,12 +7,11 @@ selector: unescape('renderOn%3A'),
 category: 'rendering',
 fn: function (html){
 var self=this;
-(function($rec){smalltalk.send($rec, "_class_", ["container"]);return smalltalk.send($rec, "_with_", [(function(){(function($rec){smalltalk.send($rec, "_id_", ["header"]);smalltalk.send($rec, "_class_", [unescape("span-24%20last")]);return smalltalk.send($rec, "_with_", [(function(div){return smalltalk.send(div, "_h1_", [smalltalk.send(self, "_projectname", [])]);})]);})(smalltalk.send(html, "_div", []));smalltalk.send(html, "_hr", []);(function($rec){smalltalk.send($rec, "_id_", ["subheader"]);smalltalk.send($rec, "_class_", [unescape("span-24%20last")]);return smalltalk.send($rec, "_with_", [(function(div){return (self['@descriptionDiv']=(function($rec){smalltalk.send($rec, "_class_", ["alt"]);smalltalk.send($rec, "_with_", ["Amber Project description"]);return smalltalk.send($rec, "_yourself", []);})(smalltalk.send(div, "_h3", [])));})]);})(smalltalk.send(html, "_div", []));smalltalk.send(html, "_hr", []);smalltalk.send(html, "_with_", ["This is your Amber project README."]);(self['@repositoryDiv']=smalltalk.send(smalltalk.send(html, "_div", []), "_id_", ["test"]));return smalltalk.send(smalltalk.send(html, "_div", []), "_with_", [(function(){return (function($rec){smalltalk.send($rec, "_class_", ["test"]);smalltalk.send($rec, "_with_", ["test get"]);return smalltalk.send($rec, "_onClick_", [(function(){return smalltalk.send(self['@repositoryDiv'], "_contents_", [(function(div){return smalltalk.send(self, "_getTest", []);})]);})]);})(smalltalk.send(html, "_button", []));})]);})]);})(smalltalk.send(html, "_div", []));
-smalltalk.send(self, "_getRepositoryJsonDo_onError_", [(function(repoJSON){return smalltalk.send(self['@descriptionDiv'], "_contents_", [(function(h){return smalltalk.send(h, "_with_", [smalltalk.send(repoJSON, "_description", [])]);})]);}), (function(status){return nil;})]);
+(function($rec){smalltalk.send($rec, "_class_", ["container"]);return smalltalk.send($rec, "_with_", [(function(){(function($rec){smalltalk.send($rec, "_id_", ["header"]);smalltalk.send($rec, "_class_", [unescape("span-24%20last")]);return smalltalk.send($rec, "_with_", [(function(div){return smalltalk.send(div, "_h1_", [smalltalk.send(self, "_projectname", [])]);})]);})(smalltalk.send(html, "_div", []));smalltalk.send(html, "_hr", []);(function($rec){smalltalk.send($rec, "_id_", ["subheader"]);smalltalk.send($rec, "_class_", [unescape("span-24%20last")]);return smalltalk.send($rec, "_with_", [(function(div){(self['@descriptionDiv']=(function($rec){smalltalk.send($rec, "_class_", ["alt"]);return smalltalk.send($rec, "_yourself", []);})(smalltalk.send(div, "_h3", [])));return smalltalk.send(self, "_repositoryJsonDo_", [(function(json){return smalltalk.send(self['@descriptionDiv'], "_with_", [smalltalk.send(json, "_description", [])]);})]);})]);})(smalltalk.send(html, "_div", []));smalltalk.send(html, "_hr", []);smalltalk.send(html, "_with_", ["This is your Amber project README."]);(self['@repositoryDiv']=smalltalk.send(smalltalk.send(html, "_div", []), "_id_", ["test"]));return smalltalk.send(smalltalk.send(html, "_div", []), "_with_", [(function(){return (function($rec){smalltalk.send($rec, "_class_", ["test"]);smalltalk.send($rec, "_with_", ["test get"]);return smalltalk.send($rec, "_onClick_", [(function(){return smalltalk.send(self['@repositoryDiv'], "_contents_", [(function(div){return smalltalk.send(self, "_getTest", []);})]);})]);})(smalltalk.send(html, "_button", []));})]);})]);})(smalltalk.send(html, "_div", []));
 return self;},
 args: ["html"],
-source: unescape('renderOn%3A%20html%0A%0A%09html%20div%20class%3A%27container%27%3B%20with%3A%5B%20%0A%09%09html%20div%0A%09%09%09id%3A%20%27header%27%3B%0A%09%09%09class%3A%20%27span-24%20last%27%3B%0A%09%09%09with%3A%20%5B%20%3Adiv%20%7C%20div%20h1%3A%20self%20projectname%20%5D.%0A%09%09html%20hr.%0A%09%09html%20div%0A%09%09%09id%3A%20%27subheader%27%3B%0A%09%09%09class%3A%20%27span-24%20last%27%3B%0A%09%09%09with%3A%20%5B%3Adiv%20%7C%20%0A%09%09%09%09descriptionDiv%20%3A%3D%20div%20h3%0A%09%09%09%09%09class%3A%20%27alt%27%3B%0A%09%09%09%09%09with%3A%20%27Amber%20Project%20description%27%3B%0A%09%09%09%09%09yourself%20%5D.%0A%09%09html%20hr.%0A%09%09html%20with%3A%20%27This%20is%20your%20Amber%20project%20README.%27%20.%0A%09%09repositoryDiv%20%3A%3D%20html%20div%20id%3A%27test%27.%0A%09%09html%20div%20with%3A%5B%20%20%20%09%0A%09%09%09html%20%0A%20%20%20%20%20%20%20%20%09%09%09button%0A%09%09%09%09class%3A%27test%27%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%09%09with%3A%20%27test%20get%27%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%09%09onClick%3A%20%5B%20%20repositoryDiv%20contents%3A%20%20%5B%3Adiv%20%7C%20self%20getTest%20%20%5D%20%5D%20%5D%20%5D.%0A%09self%20%0A%09%09getRepositoryJsonDo%3A%20%5B%3ArepoJSON%20%7C%20descriptionDiv%20contents%3A%20%5B%3Ah%20%7C%20h%20with%3A%20repoJSON%20description%20%5D%5D%20%0A%09%09onError%3A%20%5B%3Astatus%20%7C%20%5D'),
-messageSends: ["class:", "with:", "id:", "h1:", "projectname", "div", "hr", "yourself", "h3", "onClick:", "contents:", "getTest", "button", "getRepositoryJsonDo:onError:", "description"],
+source: unescape('renderOn%3A%20html%0A%0A%09html%20div%20class%3A%27container%27%3B%20with%3A%5B%20%0A%09%09html%20div%0A%09%09%09id%3A%20%27header%27%3B%0A%09%09%09class%3A%20%27span-24%20last%27%3B%0A%09%09%09with%3A%20%5B%20%3Adiv%20%7C%20div%20h1%3A%20self%20projectname%20%5D.%0A%09%09html%20hr.%0A%09%09html%20div%0A%09%09%09id%3A%20%27subheader%27%3B%0A%09%09%09class%3A%20%27span-24%20last%27%3B%0A%09%09%09with%3A%20%5B%3Adiv%20%7C%20%0A%09%09%09%09descriptionDiv%20%3A%3D%20div%20h3%0A%09%09%09%09%09class%3A%20%27alt%27%3B%0A%09%09%09%09%09yourself.%0A%09%09%09%09self%20repositoryJsonDo%3A%20%5B%3Ajson%20%7C%20descriptionDiv%20with%3A%20json%20description%20%5D%5D.%0A%09%09html%20hr.%0A%09%09html%20with%3A%20%27This%20is%20your%20Amber%20project%20README.%27%20.%0A%09%09repositoryDiv%20%3A%3D%20html%20div%20id%3A%27test%27.%0A%09%09html%20div%20with%3A%5B%20%20%20%09%0A%09%09%09html%20%0A%20%20%20%20%20%20%20%20%09%09%09button%0A%09%09%09%09class%3A%27test%27%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%09%09with%3A%20%27test%20get%27%3B%0A%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%09%09onClick%3A%20%5B%20%20repositoryDiv%20contents%3A%20%20%5B%3Adiv%20%7C%20self%20getTest%20%20%5D%20%5D%20%5D%20%5D.%0A'),
+messageSends: ["class:", "with:", "id:", "h1:", "projectname", "div", "hr", "yourself", "h3", "repositoryJsonDo:", "description", "onClick:", "contents:", "getTest", "button"],
 referencedClasses: []
 }),
 smalltalk.AmberProjectPage);
@@ -82,6 +81,23 @@ return self;},
 args: ["aBlock", "errorBlock"],
 source: unescape('getRepositoryJsonDo%3A%20aBlock%20onError%3A%20errorBlock%0A%0A%09jQuery%20%0A%09%09ajax%3A%20%27https%3A/api.github.com/repos/%27%2C%20self%20username%2C%20%27/%27%2C%20self%20projectname%0A%09%09options%3A%20%23%7B%20%0A%09%09%09%27type%27%20-%3E%20%27GET%27.%0A%09%09%09%27dataType%27%20-%3E%20%27jsonp%27.%0A%09%09%09%27success%27%20-%3E%20%5B%3ArepositoryData%20%3Astatus%20%3AjqXHR%20%7C%20%0A%09%09%09%09aBlock%20value%3A%20repositoryData%20data%20%20%5D.%0A%20%20%20%20%20%09%09%09%27error%27%20-%3E%5B%3AjqXHR%20%3Astatus%20%3Aerror%20%7C%20%0A%09%09%09%09errorBlock%20value%3A%20status%20printString%20%5D%7D.'),
 messageSends: ["ajax:options:", unescape("%2C"), "username", "projectname", unescape("-%3E"), "value:", "data", "printString"],
+referencedClasses: []
+}),
+smalltalk.AmberProjectPage);
+
+smalltalk.addMethod(
+unescape('_repositoryJsonDo_'),
+smalltalk.method({
+selector: unescape('repositoryJsonDo%3A'),
+category: 'accessing',
+fn: function (aBlock){
+var self=this;
+(($receiver = self['@repositoryJson']) == nil || $receiver == undefined) ? (function(){return smalltalk.send(self, "_getRepositoryJsonDo_onError_", [(function(repoJSON){(self['@repositoryJson']=repoJSON);return smalltalk.send(aBlock, "_value_", [self['@repositoryJson']]);}), (function(status){return nil;})]);})() : $receiver;
+smalltalk.send(smalltalk.send(aBlock, "_value", []), "_repositoryJson", []);
+return self;},
+args: ["aBlock"],
+source: unescape('repositoryJsonDo%3A%20aBlock%0A%0A%09repositoryJson%20ifNil%3A%20%5B%0A%09%09self%20%0A%09%09%09getRepositoryJsonDo%3A%20%5B%3ArepoJSON%20%7C%20%0A%09%09%09%09repositoryJson%20%3A%3D%20%20repoJSON.%0A%09%09%09%09aBlock%20value%3A%20repositoryJson%20%20%20%5D%20%0A%09%09%09onError%3A%20%5B%3Astatus%20%7C%20%20%5D%20%5D.%0A%09aBlock%20value%20repositoryJson%0A%09'),
+messageSends: ["ifNil:", "getRepositoryJsonDo:onError:", "value:", "repositoryJson", "value"],
 referencedClasses: []
 }),
 smalltalk.AmberProjectPage);
